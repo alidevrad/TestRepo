ceb3613d32ce9a50323ee96df1e3cb42728258c5
ali <a@a.com>
2020-01-09 01:36:33 -0800

    Add property

--- WindowsFormsApp1/Person.cs
+++ WindowsFormsApp1/Person.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WindowsFormsApp1
+﻿namespace WindowsFormsApp1
 {
     public class Person
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-
+        public int Age { get; set; }
     }
 }
